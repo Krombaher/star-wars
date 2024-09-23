8ce53f8f9d3f06ece293b3a0cb57eed220e910dd
Vitali Kazhura <v.kazhura@slotegrator.space>
2024-09-23 09:32:43 +0300

    add select new title

--- src/pages/HomePage/components/NewTitle.tsx
+++ src/pages/HomePage/components/NewTitle.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { MainSelect } from './MainSelect';
 
 export const NewTitle = () => {
   return (
     <>
       <div>NewTitle</div>
       <p>this is new title</p>
+      <MainSelect />
       <button>Click</button>
     </>
   );
