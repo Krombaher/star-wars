904e0cdae6ce6d13a015003bd3ba09f5d360c676
Vitali Kazhura <v.kazhura@slotegrator.space>
2024-09-23 09:31:09 +0300

    add select

---
+++ src/pages/HomePage/components/MainSelect.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const MainSelect = () => {
+  return (
+    <div>
+        <select>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+    </div>
+  )
+}
+
